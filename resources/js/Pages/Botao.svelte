3a35bd6438653e15a8a6707418d5cc2f560a35a2
yo <yohanhxh@gmail.com>
2023-07-22 15:47:02 -0300

    SetUp: svelte with inertia

---
+++ resources/js/Pages/Botao.svelte
@@ -0,0 +1,11 @@
+<script>
+    let number = 0; 
+
+    function addButton() {
+        number += 1;
+    }
+</script>
+
+<button on:click={addButton}>
+    Vezes clicadas: {number}
+</button>
---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Botao from "./Botao.svelte";
+</script>
+
+<main>
+    <Botao />
+</main>
